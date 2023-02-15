911f30269389ef3d7cdc2eb0245b92b0a8739106
puripuri2100 <puripuri2100@gmail.com>
2023-02-15 19:39:13 +0900

    update

--- src/environment.rs
+++ src/environment.rs
@@ -1,24 +1,36 @@
 pub type Env<T> = Vec<(String, T)>;
 
 pub fn empty<T>() -> Env<T> {
-  Vec::new()
+    Vec::new()
 }
 
-pub fn extend<T>(x: String, v: T, env: Env<T>) -> Env<T> {
-  let mut mut_env = env;
-  mut_env.push((x, v));
-  mut_env
+pub fn extend<T>(x: String, v: T, mut env: Env<T>) -> Env<T> {
+    env.push((x, v));
+    env
 }
 
 pub fn lookup<T>(x: String, env: Env<T>) -> Option<T>
 where
-  T: Clone,
+    T: Clone,
 {
-  let mut env_mut = env;
-  env_mut.reverse();
-  let a = env_mut.iter().find(|(y, _)| &x == y);
-  match a {
-    None => None,
-    Some((_, v)) => Some(v.clone()),
-  }
+    let mut env_mut = env;
+    env_mut.reverse();
+    let a = env_mut.iter().find(|(y, _)| &x == y);
+    a.map(|(_, v)| v.clone())
+}
+
+pub fn remove<T>(id: &str, env: Env<T>) -> Env<T> {
+    let mut l = Vec::new();
+    let mut e = env;
+    let mut is_find = false;
+    while let Some(t) = e.pop() {
+        let (t_id, _) = &t;
+        if t_id == id && !is_find {
+            is_find = true
+        } else {
+            l.push(t);
+        }
+    }
+    l.reverse();
+    l
 }
--- src/main.rs
+++ src/main.rs
@@ -9,39 +9,38 @@ pub mod parser;
 pub mod types;
 
 fn main() {
-  let app = App::new("ralcu").version("0.0.1").arg(
-    Arg::with_name("input")
-      .help("Specify input file")
-      .value_name("FILE")
-      .takes_value(true),
-  );
-  let matches = app.get_matches();
-  let mut input_file_name = matches.value_of("input").unwrap();
-  let mut f = File::open(&mut input_file_name).unwrap();
-  let mut contents = String::new();
-  f.read_to_string(&mut contents).unwrap();
-  println!("file name: {}", input_file_name);
-  a(&mut contents);
+    let app = App::new("ralcu").version("0.0.1").arg(
+        Arg::with_name("input")
+            .help("Specify input file")
+            .value_name("FILE")
+            .takes_value(true),
+    );
+    let matches = app.get_matches();
+    let input_file_name = matches.value_of("input").unwrap();
+    let mut f = File::open(input_file_name).unwrap();
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).unwrap();
+    println!("file name: {input_file_name}");
+    a(&contents);
 }
 
 #[allow(dead_code)]
 fn a(s: &str) {
-  println!(" --- --- ---\n{}\n --- --- ---", s);
-  let env = environment::empty();
-  let tokens = lexer::lex(s).unwrap();
-  let ast_res = parser::parse(tokens);
-  //println!("{:?}", ast_res);
-  let ast = ast_res.unwrap();
-  let (id, newenv, v) = eval::eval_decl(env, ast);
-  println!("val {} =", id);
-  println!("{:?}", v);
-  //println!("{:?}", newenv);
+    println!(" --- --- ---\n{s}\n --- --- ---");
+    let env = environment::empty();
+    let tokens = lexer::lex(s).unwrap();
+    let ast_res = parser::parse(tokens);
+    //println!("{:?}", ast_res);
+    let ast = ast_res.unwrap();
+    let (_id, _newenv, v) = eval::eval_decl(env, ast);
+    println!("-> {}", eval::string_of_exval(v));
+    //println!("{:?}", newenv);
 }
 
 #[allow(dead_code)]
 fn b(s: &str) {
-  println!(" --- --- ---\n{}\n --- --- ---", s);
-  let tokens = lexer::lex(s).unwrap();
-  let ast_res = parser::parse(tokens);
-  println!("{:?}", ast_res);
+    println!(" --- --- ---\n{s}\n --- --- ---");
+    let tokens = lexer::lex(s).unwrap();
+    let ast_res = parser::parse(tokens);
+    println!("{ast_res:?}");
 }
